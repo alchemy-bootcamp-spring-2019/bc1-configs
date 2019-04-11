b6dd5df6ccce6d4f8b2e5e666fc3724d1527eb05
Paige Gorry <paigeegorry@gmail.com>
2019-04-11 11:09:00 -0700

    truthTeller demo

--- test/tests.js
+++ test/tests.js
@@ -1,5 +1,17 @@
 const test = QUnit.test;
 
-test('time to write a test', function(assert) {
-    assert.equal(true, false);
+function truthTeller(trueOrFalse) {
+    return trueOrFalse;
+}
+
+test('function returns true or false', function(assert) {
+    //Arrange
+    const truthyThing = true;
+    const expectedResult = true;
+
+    //Act 
+    const functionResult = truthTeller(truthyThing);
+
+    //Assert
+    assert.equal(functionResult, expectedResult);
 });
